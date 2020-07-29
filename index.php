f8171f83dc39fe47866b93b915fd8083fd16299f
Albrit197 <63236021+Albrit197@users.noreply.github.com>
2020-07-30 00:45:16 +0300

    final

--- index.php
+++ index.php
@@ -13,22 +13,12 @@
               $message = 'Wrong username or password';
        }
     }
-         
-            if(isset($_GET['action']) and $_GET['action'] == 'logout'){
+         if(isset($_GET['action']) and $_GET['action'] == 'logout'){
               session_start();
               unset($_SESSION['username']);
               unset($_SESSION['password']);
               unset($_SESSION['logged_in']);
-              $message = '';
-         if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {	
-           if ($_POST['username'] == 'Wellboy' && $_POST['password'] == 'koja') {
-              $_SESSION['logged_in'] = true;
-              $_SESSION['username'] = 'Wellboy';
-           } else {
-              $message = 'Wrong username or password';
-       }
-    }
-              
+              header("Location: index.php");   
         }
 
                           //Create dir
@@ -36,11 +26,12 @@
         if(isset($_GET["create_dir"])){
          if($_GET["create_dir"] != ""){
            $NewDir = './' . $_GET["path"] . $_GET["create_dir"];
-               if (!is_dir($NewDir)){
+               if (!is_dir($NewDir))
                  mkdir($NewDir, 0777, true);
-                 echo "<meta http-equiv='refresh' content='0'>";
-             }   
+               //   echo "<meta http-equiv='refresh' content='0'>"; // Refresh page logic    
            }
+           $url = preg_replace("/(&?|\??)create_dir=(.+)?/", "", $_SERVER["REQUEST_URI"]);
+    header('Location: ' . urldecode($url)); 
          }
 
                           //Delete file
@@ -55,6 +46,53 @@
                }
             }
          }
+                           // Download file
+
+if (isset($_POST['download'])) {
+   print('Path to download: ' . str_replace('?path=/', '', $_SERVER['REQUEST_URI']) . $_POST['download'] . ' ----------> ' . "File has been successfully downloaded.");
+   $file = './' . $_GET["path"] . $_POST['download'];
+   $file_download = str_replace("&nbsp;", " ", htmlentities($file, null, 'utf-8'));
+
+   header('Content-Description: File Transfer');
+   header('Content-Type: application/pdf');
+   header('Content-Disposition: attachment; filename=' . basename($file_download));
+   header('Content-Transfer-Encoding: binary');
+   header('Expires: 0');
+   header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+   header('Pragma: public');
+   header('Content-Length: ' . filesize($file_download));
+
+   readfile($file_download);
+   exit;
+}
+
+                             //Upload file
+
+if (isset($_FILES['fileUpload'])) {
+   $errors = array();
+   $file_name = $_FILES['fileUpload']['name'];
+   $file_size = $_FILES['fileUpload']['size'];
+   $file_tmp = $_FILES['fileUpload']['tmp_name'];
+   $file_type = $_FILES['fileUpload']['type'];
+   $file_ext = strtolower(end(explode('.', $_FILES['fileUpload']['name'])));
+
+   $file_extension = array("txt");
+
+   if (in_array($file_ext, $file_extension) === false) {
+       $errors[] = "extension not allowed, please choose only .txt file.";
+   }
+
+   if ($file_size > 2097152) {
+       $errors[] = 'File size must be below 2 MB';
+   }
+
+   if (empty($errors) == true) {
+       move_uploaded_file($file_tmp, './' . $_GET["path"] . $file_name);
+   } else {
+       print_r($errors);
+   }
+}
+
       ?>
  <!DOCTYPE html>
 <html lang="en">
@@ -80,7 +118,7 @@
           die();
    }
         
-                       // FILE BROWSER
+                       // File browser
 
       $path = './' . $_GET["path"];
       $fileDir = scandir($path);
@@ -94,8 +132,8 @@
             print('<td class="value">' . (is_dir($path . $content) ? "Directory" : "File") . '</td>');
             print('<td class="value">' . (is_dir($path . $content)? 
                   '<a href="' . (isset($_GET['path'])
-                  ? $content . '/' 
-                  : '?path=' . $content . '/') . '">' . $content . '</a>'
+                  ? $_SERVER['REQUEST_URI']. $content . '/' 
+                  : $_SERVER['REQUEST_URI']. '?path=' . $content . '/') . '">' . $content . '</a>'
                      : $content) 
                         . '</td>');
             print('<td class="value">'. (is_dir($path . $content)? ''
@@ -103,19 +141,25 @@
                         <input type="hidden" name="delete" value=' . str_replace(' ', '&nbsp;', $content) . '>
                         <input class="delete" type="submit" value="Delete"></form>'). "</td>");
             print('</tr>');
+            
         }
     }
     print("</table>"); 
-                                      
-                                 //Go back button
+    ?> 
 
-    $link_back = filter_var($_SERVER['HTTP_REFERER'], FILTER_VALIDATE_URL);
-    if (!empty($link_back)) {
-       echo '<p class="margin"><a class="back" href="'. $link_back .'" title="Return to the previous page">&laquo; Go back</a></p>';
-          } else {
-             echo '<p class="back"><a class="field" href="javascript:history.go(-1)" title="Return to the previous page">&laquo; Go back</a></p>';
-    }                
-     ?>            
+                                      
+                                 <!-- Go back button -->
+
+    <button class="back">
+            <a href="<?php
+                        $back = explode('/', rtrim($_SERVER['QUERY_STRING'], '/'));
+                        array_pop($back);
+                        count($back) == 0
+                            ? print('?path=/')
+                            : print('?' . implode('/', $back) . '/');
+                        ?>">Back to the previous page</a>
+        </button>
+              
        <form  action="/SauliusBrowser" method="get"> 
           <div class="create">
             <input type="hidden" name="path" value="<?php print($_GET['path']) ?>" /> 
